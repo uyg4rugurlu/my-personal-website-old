c51536e91d210f4be81787f077c2e6c8490371ea
Uygar Uğurlu <115646619+uyg4rugurlu@users.noreply.github.com>
2022-11-02 19:05:21 +0300

    Ufak bir güncelleme yapıldı.
    4 o 4  - >  404

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       <Seo />
       <div className="flex flex-col items-center justify-center w-screen h-screen">
         <h1 className="text-4xl font-bold tracking-tight mb-2 dark:text-white">
-          4 o 4
+          4 0 4
         </h1>
         <LinkTag href="/">&larr; Ah shit, here we go again.</LinkTag>
       </div>
