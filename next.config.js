e4903f4d8f09f5c4aac3e4435f835c7124a2c86b
Uygar Uğurlu <115646619+uyg4rugurlu@users.noreply.github.com>
2023-02-11 21:28:17 +0300

    Update next.config.js
    Hidden source code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+    reactStrictMode: false,
+    productionBrowserSourceMaps: false,
+    webpack(config, options) {
+        if (options.isServer) {
+            config.devtool = 'hidden-source-map';
+        }
+        return config;
+    }
 };
 
 module.exports = nextConfig;
